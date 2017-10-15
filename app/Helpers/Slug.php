0f57edfef83d3864d1fbed4f76544b1bb2288019
Adrian <adrianbarylski@icloud.com>
2017-10-15 17:39:57 +0100

    Updated the generator to allow a 3rd param.

--- app/Helpers/Slug.php
+++ app/Helpers/Slug.php
@@ -10,11 +10,12 @@ class Slug
      *
      * @param class $model
      * @param string $title
+     * @param class $thisModel = NULL
      * @return string $slug | $newSlug
      * @throws \Exception
      *
     */
-    public function generate($model, $title)
+    public function generate($model, $title, $thisModel = NULL)
     {
         // Generate the slug from a title
         $slug = str_slug($title);
@@ -24,6 +25,11 @@ class Slug
         if(!$all->contains('slug', $slug)) {
             return $slug;
         }
+        // Check whether the $slug exists in the array and if the slug is the same as the one in the current model
+        // If this is true, it means that there's no need to generate a new slug
+        if($all->contains('slug', $slug) && $thisModel !== NULL && $thisModel->slug === $slug) {
+            return $slug;
+        }
 
         // Set the tries to the number of returned slugs.
         // Adding 1 fixes the problem of runnig out of numbers to try against
@@ -31,8 +37,15 @@ class Slug
         for($i = 1; $i <= $tries; $i++) {
             // Append a hyphen and the number to the generated slug using str_slug function
             $newSlug = $slug . '-' . $i;
-            // Return the $newSlug if it isn't in our array of slugs ($all) from the DB
-            if(!$all->contains('slug', $newSlug)) {
+            // Check whether $thisModel is not NULL
+            if($thisModel !== NULL) {
+                // If the $newSlug is the same as the current model slug
+                if($newSlug === $thisModel->slug) {
+                    return $newSlug;
+                }
+                // If the current model is null and the $newSlug is not in the array
+                // Essentially means to generate a completely new slug
+            } else if($thisModel === NULL && !$all->contains('slug', $newSlug)) {
                 return $newSlug;
             }
         }
@@ -52,7 +65,7 @@ class Slug
     */
     protected function find($model, $slug)
     {
-        return $model::select('slug')->where('slug', 'LIKE', '%'.$slug.'%')->get();
+        return $model::select('id','slug')->where('slug', 'LIKE', '%'.$slug.'%')->get();
     }
 
 }
